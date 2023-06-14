0a5055056893440854a9f8ba93fb3a2d5e77328e
vladywill <rails.premium@gmail.com>
2023-06-14 18:40:11 -0400

    fog-aws and S3 bucket connection 0.2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider:              'AWS',                        
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], 
-    region:                'us-east-1',                  
+    provider: 'AWS',
+    region: 'us-east-1',  # Замените на регион вашего ведра
+    use_iam_profile: true
   }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']  
+  config.fog_directory  = ENV['S3_BUCKET_NAME']
+  config.fog_public     = false  # Опционально - для приватного ведра
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
 end
