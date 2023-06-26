aecd94567e6b89bc1f1f09268ee59f0c92fc5212
vladywill <rails.premium@gmail.com>
2023-06-26 07:18:53 -0400

    styles for https 0.10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # config.action_controller.asset_host = "`http`s://s3.amazonaws.com/gotham-pictures"
+  # config.action_controller.asset_host = "https://s3.amazonaws.com/gotham-pictures"
   # картинки с S3 AWS
 
   # Settings specified here will take precedence over those in config/application.rb.
@@ -25,13 +25,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
