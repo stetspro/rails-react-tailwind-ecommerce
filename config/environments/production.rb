aac86b0d8b6ea4a44801fa7caeda20c047a61476
vladywill <rails.premium@gmail.com>
2023-06-25 22:47:25 -0400

    styles for https 0.5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,13 +25,13 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  # config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
